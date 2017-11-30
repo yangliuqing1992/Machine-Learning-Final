9eea08b5fcc60a632789199ef6bd6473cbb0b66c
yangliuqing1992 <34041961+yangliuqing1992@users.noreply.github.com>
2017-11-30 17:34:51 -0500

    Add files via upload
    train test is separated from the original data and train_nmla test_nmlz from normalized data.

--- preprocessing.R
+++ preprocessing.R
@@ -17,5 +17,22 @@ nmlz=function(x){
 tt_nmlz=apply(tt[,-c(1,2)],2,nmlz)
 tt_nmlz=cbind.data.frame(tt[,c(1,2)],tt_nmlz)
 
-write.csv(tt_nmlz,file = "data_nmlz.csv")#extract normalized data out.
+write.csv(tt_nmlz,file = "data_nmlz.csv",row.names = F)#extract normalized data out.
+
+#sepereate data for training and testing
+n=length(tt_nmlz[,1])
+n_train=floor(n*0.8) #let 80% in the original data for training.
+n_test=n-n_train
+set.seed(1234)
+indx=sample(seq(1,n),n_train)
+train=tt[indx,]
+test=tt[-indx,]
+train_nmlz=tt_nmlz[indx,]
+test_nmlz=tt_nmlz[-indx,]
+write.csv(train,file="train.csv",row.names=F)
+write.csv(test,file="test.csv",row.names = F)
+write.csv(train_nmlz,file="train_nmlz.csv",row.names=F)
+write.csv(test_nmlz,file="test_nmlz.csv",row.names = F)
+
+
 
